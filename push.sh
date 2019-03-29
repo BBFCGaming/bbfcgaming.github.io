5a8e3bb2ea8da6ceeac04408b60334369a77b4b2
Finian Wright <finianwright@gmail.com>
2019-03-29 13:41:22 -0400

    Pushed from Yoda

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 cm="Pushed from `hostname`"
 echo "Adding..."
 git add --all
-echo "Committing with message:" $cm
+echo "Committing with message:" $cm"..."
 git commit -m "$cm"
 echo "Pushing to server..."
 git push
