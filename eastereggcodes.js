464cd202393106dd9cf634b7949663d70208ded3
BBFC Gaming <33670917+BBFCGaming@users.noreply.github.com>
2019-09-17 15:34:32 -0400

    Update eastereggcodes.js

--- eastereggcodes.js
+++ eastereggcodes.js
@@ -1,1 +1,202 @@
+var pass;
+    function eastereggcode() {
+        //var reason = why("are you looking here")
+        //if (reason == "To get codes") {
+        //  you="Cheater"
+        //}
+        pass = prompt("CODE");
+        if (pass == "^^vv<><>ba") {
+            $("cheatbanner").html('<div><h3 style="border: 5px solid red"><center>Cheat Mode</center></h3></div>');
+            cheat = 1
+        }
+        if (pass == "BBFC") {
+            var audio = new Audio('BBFC Gaming Theme.wav');
+            audio.play();
+        }
+        if (pass == "I'm bored") {
+            window.alert('How can you be bored while only one click away from the best games ever? Just play the featured game below, or click "Flash Games" or "HTML5 Games" to see a list of games!');
+        }
+        if (pass == "BBFC.apk") {
+            window.alert("Click OK to download the beta version of our app, which will allow you to scan special QR codes for secret games.");
+            window.location.replace("https://bbfcgaming.github.io/gs/BBFC.apk");
+        }
+        if (pass == "cheese" ) {
+            window.location.replace("cheese.png");
+        }
+        if (pass == "42") {
+            window.alert("Correct!");
+        }
+        if (pass == "43") {
+            window.alert("Close, but not quite.");
+        }
+        if (pass == "AsteroidsAreFun") {
+            window.alert("Website Asteroids (http://www.websiteasteroids.com/)");
+            var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='asteroids.min.js';void(0);
+        }
+            if (pass == "AllWillPerish") {
+                window.alert("ALL");
+                window.alert("WILL");
+                window.alert("PERISH!!!!!!");
+                $("perishable").html('<blink><div style="text-decoration: blink;"><h1 style="border: 5px solid red">Ä̸̬̥͔͍̥̜̯́ͬ́́ͬl̟͓͕ͭͦ͘͝͝l̸̳̖ͪ̎̉ ̷̘̦͔̪̓͛̽̌̎W̡̯̝͖̬̠͎̞͗̆̇͜į̡̝͛̒͊ͬ͏̮ͥ̏l̷̢̨͕̼̺̗̺̦̯̯̓̄̋ͫ͌ͦ̇ͣ͞l͈̆͌ ̶͙͚͕̼̪͐̄̌ͧ̋͆P̸̯͓͛ͥ̑̐é̸̞̓ͤ͌ͩ͘͜͞͝r̹ͤ́͒͢i̞̍́̆͛̆ͫ̃҉̦̙̈̄́̆̂̕͞s̳̭̝h̩͉̙̘̯ͬ́̂̉͗̒͋͞ͅ</h1></div></blink>');
+                var audio = new Audio('BBFC Gaming Theme.wav');
+                audio.play();
+                var audio = new Audio('whitenoise.mp3');
+                audio.play();
+            }
+        if (pass == "0xa1423b") {
+            window.alert("You looked at the source didn't you");
+            window.alert("I mean, how else would you have known to type in '0xa1423b'?");
+            window.alert("Unless you were just using brute force");
+            window.alert("Typing in random combinations of characters");
+            window.alert("What did you want to achieve?");
+            window.alert("Did you expect a secret game?");
+            window.alert("Or a prize for your effort?");
+            window.alert("Maybe you were just bored");
+            window.alert("But you must have been really bored to spend hours typing random characters into a text box");
+            window.alert("Or maybe you did just look in the source after all");
+            window.alert("It would be so easy...");
+            window.alert("You would have to have so much time if you wanted to use brute force...");
+            window.alert("Maybe you wanted to waste your time..");
+            window.alert("Clicking through JavaScript alerts...");
+            window.alert("For so long...");
+            window.alert("So long...");
+            window.alert("So long...");
+            window.alert("So long...");
+            window.alert("So long...");
+            window.alert("Maybe you just found the code box...");
+            window.alert("And just typed this in at random on your first try");
+            window.alert("...");
+            window.alert("...");
+            window.alert("...");
+            window.alert("...");
+            window.alert("...");
+            window.alert("...");
+            window.alert("...");
+            window.alert("...");
+            window.alert("...");
+        }
+        if (pass == "DeAtH_MuSiC") {
+            window.alert("I'm warning you beforehand.");
+            window.alert("This is death music.");
+            window.alert("Do not use earbuds.");
+            window.alert("Okay, we both know you ignored that. But seriously, don't.")
+            window.alert("Once you click Okay, the music will start");
+            var audio = new Audio('DEATH.wav');
+            audio.play();
+        }
+        if (pass == "Secret Game") {
+            window.alert("Hi.");
+             window.alert("Do you like potatoes?");
+            window.alert("I do.");
+            window.alert("Did you really think the code Secret Game would give you a secret game?");
+            window.alert("Maybe it would eat your credit card.");
+            window.alert("Or corrupt all of your files.");
+            window.alert("You want a secret game?");
+            window.alert("Ok then...");
+            window.location.replace("bbfcgaming.github.io/SecretGameBBFC.html");
+        }
+        if (pass == "Dialouge") {
+            window.alert("D");
+            window.alert("Di");
+            window.alert("Dia");
+            window.alert("Dial");
+            window.alert("Dialo");
+            window.alert("Dialou");
+            window.alert("Dialoug");
+            window.alert("Dialouge");
+            window.alert("Dialoug");
+            window.alert("Dialou");
+            window.alert("Dialo");
+            window.alert("Dial");
+            window.alert("Dia");
+            window.alert("Di");
+            window.alert("D");
+            window.alert("Di");
+            window.alert("Dia");
+            window.alert("Dial");
+            window.alert("Dialo");
+            window.alert("Dialou");
+            window.alert("Dialoug");
+            window.alert("Dialouge");
+            window.alert("Dialoug");
+            window.alert("Dialou");
+            window.alert("Dialo");
+            window.alert("Dial");
+            window.alert("Dia");
+            window.alert("Di");
+            window.alert("D");
+            window.alert("D");
+            window.alert("Di");
+            window.alert("Dia");
+            window.alert("Dial");
+            window.alert("Dialo");
+            window.alert("Dialou");
+            window.alert("Dialoug");
+            window.alert("Dialouge");
+            window.alert("Dialoug");
+            window.alert("Dialou");
+            window.alert("Dialo");
+            window.alert("Dial");
+            window.alert("Dia");
+            window.alert("Di");
+            window.alert("D");
+        }
+        if (pass == "Chicken ancestor") {
+         window.location.replace("dino.html");
+        }
+        if (pass == "goodbye" ) {
+            window.alert("Goodbye");
+            window.close();
+        }
+          if (pass == "Bags of Carpet" ) {
+            window.location.replace("Carpetbagger.png");
+        }
+         if (pass == "BBFCraft") {
+            window.location.replace("BBFCraft-20190112T011323Z-001.zip");
+        }
+        if (pass == "The green one is green") {
+            window.alert("Yes it is");
+            var audio = new Audio('crabrave.mp3');
+            audio.play();
+        }
+        if (pass == "If/Then") {
+            window.alert("Here is an if then statement:");
+            window.alert("If it is green, then it is blue");
+            window.alert("If it is green - p | If it is blue - q");
+            window.alert("p🡒q");
+            window.alert("~p🡒~q");
+        }
+        if (pass == "cmd.exe"){
+            window.location.replace("console.html")
+        }
+        if (pass == "I'm writing a letter"){
+            clippy.load('Clippy', function(agent){
+                //agent.show();
+                agent.stop();
+                agent.moveTo(0, 0);
+                agent.play('Greeting');
+                agent.speak("It looks like you're writing a letter. Would you like help?");
+            });
+        }
+        if (pass == "coffee"){
+            location.replace("418.html");
+        }
+        /*if (pass == "plants"){
+             clippy.load('Bonzi', function(agent){
+                //agent.show();
+                //agent.stop();
+                //agent.play("MoveLeft");
+                agent.show();
+                agent.play("Greeting");
+                //alert("You might have to close the net neutrality warning in the corner to see me.")
+                agent.speak("Well! Hello there!");
+                agent.speak("I don't believe we've been properly introduced.");
+                agent.speak("I'm Bonzi");
+                agent.speak("What's your name?");
+                agent.moveTo(100000,10000);
 
+            });
+            }*/
+    }
+
+  
